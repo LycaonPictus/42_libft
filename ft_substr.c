119e642b2aa0da27f4e1842638da5f883f6de27d
John Eric Holland Asensio <jholland@c1r13s2.42malaga.com>
2023-12-03 17:50:49 +0100

    +substr

---
+++ ft_substr.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jholland <jholland@student.42malaga.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/03 17:34:05 by jholland          #+#    #+#             */
+/*   Updated: 2023/12/03 17:34:06 by jholland         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*sub;
+	size_t	i;
+	size_t	strlen;
+
+	strlen = ft_strlen(s);
+	if (start > strlen)
+		len = 0;
+	else if (start + len > strlen)
+		len = strlen - start;
+	sub = malloc(len + 1);
+	if (!sub)
+		return (NULL);
+	s += start;
+	i = 0;
+	while (i < len)
+	{
+		sub[i] = s[i];
+		i++;
+	}
+	sub[i] = '\0';
+	return (sub);
+}
