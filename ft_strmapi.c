736a130ac33ab14a3c03635e5f476e3db397105c
John Eric Holland Asensio <jholland@c1r13s6.42malaga.com>
2023-12-05 13:09:12 +0100

    update

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -3,10 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strmapi.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jholland <jholland@student.42malaga.com>   +#+  +:+       +#+        */
+/*   By: jholland <jholland@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/04 21:48:33 by jholland          #+#    #+#             */
-/*   Updated: 2023/12/04 21:48:36 by jholland         ###   ########.fr       */
+/*   Updated: 2023/12/05 13:06:58 by jholland         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char			*out;
+	unsigned int	index;
+
+	out = malloc(ft_strlen(s) + 1);
+	if (!out)
+		return (out);
+	index = 0;
+	while (s[index])
+	{
+		out[index] = f(index, s[index]);
+		index++;
+	}
+	out[index] = '\0';
+	return (out);
+}
